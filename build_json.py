cc7e70b1e235ed396f8dfe2c3494dc21194b246c
amay077 <okuokuoku@gmail.com>
2021-01-04 00:19:21 +0900

    住居地 と 年代-性別 の集計値を追加

--- build_json.py
+++ build_json.py
@@ -12,7 +12,10 @@ JST_current_time = datetime.now(tz=JST).strftime('%Y/%m/%d %H:%M')
 
 
 patients_list = []
-patients_summary_dic = {}
+patients_date_num_dic = {}
+patients_date_place_dic = {}
+patients_date_age_sex_dic = {}
+patients_date_sex_dic = {}
 
 # 引数を取得 異常系処理はしてないので注意
 args = sys.argv
@@ -21,8 +24,24 @@ with open('data/patients.csv', 'r', encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         patients_list.append(row)
-        patients_summary_dic.setdefault(row['date'], 0)
-        patients_summary_dic[row['date']] += 1
+        dt = row['date']
+        patients_date_num_dic.setdefault(dt, 0)
+        patients_date_num_dic[dt] += 1
+
+        patients_date_place_dic.setdefault(dt, {})
+        placeDic = patients_date_place_dic[dt]
+        placeDic.setdefault(row['住居地'], 0)
+        placeDic[row['住居地']] += 1
+
+        sex = row['年代・性別'][-2:]
+        age = row['年代・性別'].replace(sex, '')
+        patients_date_age_sex_dic.setdefault(dt, {})
+        ageDic = patients_date_age_sex_dic[dt]
+        ageDic.setdefault(age, {})
+        sexDic = ageDic[age]
+        sexDic.setdefault(sex, 0)
+        sexDic[sex] += 1
+
 
 # 日付のリストを生成
 strdt = datetime.strptime("2020-01-26", '%Y-%m-%d')  # 開始日
@@ -40,14 +59,20 @@ patients_summary_list = []
 # 日付の新しい順に辿って小計が 0 でない日から開始する
 foundZero = True
 for date in reversed(datelist):
-    if (not (date.strftime('%Y-%m-%d') in patients_summary_dic)) and foundZero:
+    dtKey = date.strftime('%Y-%m-%d')
+    if (not (dtKey in patients_date_num_dic)) and foundZero:
         continue
     else:
         foundZero = False
-        patients_summary_dic.setdefault(date.strftime('%Y-%m-%d'), 0)
+        patients_date_num_dic.setdefault(dtKey, 0)
+        patients_date_place_dic.setdefault(dtKey, {})
+        patients_date_age_sex_dic.setdefault(dtKey, {})
+        patients_date_sex_dic.setdefault(dtKey, {})
         patients_summary_list.append({
-            "日付": date.strftime('%Y-%m-%d'),
-            "小計": patients_summary_dic[date.strftime('%Y-%m-%d')]
+            "日付": dtKey,
+            "小計": patients_date_num_dic[dtKey],
+            "住居地": patients_date_place_dic[dtKey],
+            "年代": patients_date_age_sex_dic[dtKey],
         })
 
 patients_summary_list = patients_summary_list[::-1] # 日付の昇順に並び替え
